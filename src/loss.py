69d43b52509c9ad3039c289da26712a3faa2acc0
Hoofar Pourzand <support@lotustech.io>
2020-02-26 14:04:38 -0500

    updates loss

---
+++ src/loss.py
@@ -0,0 +1,11 @@
+def loss():
+    """
+    input
+    output
+    logic
+    """
+    print("loss callback")
+
+if __name__ == "__main__":
+    loss = loss()
+    #loss.to_csv(f"logs/{MODEL}.csv", index=False)
