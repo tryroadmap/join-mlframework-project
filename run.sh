3a50f1a50cfdb19500193d41eb703fc0d64d3e0e
Logan Gantner <logangantner@gmail.com>
2020-03-05 13:24:45 -0500

    created params input, implemented temporary data split functionality

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
-export TRAINING_DATA=input/train_folds.csv
+# export TRAINING_DATA=input/train_folds.csv
+export TRAINING_DATA=input/train.csv
 export TEST_DATA=input/test.csv
+export PARAMS=input/params.json
 
 export MODEL=$1
 
 FOLD=0 python -m src.train
 #add more folds
-python -m src.predict
+# python -m src.predict
--- src/train.py
+++ src/train.py
@@ -1,18 +1,21 @@
 import os
+import json
 import pandas as pd
-from sklearn import ensemble
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+# from sklearn import ensemble
 from sklearn import metrics
 import joblib
 
 from . import dispatcher
 
-TRAINING_DATA = os.environ.get("TRAINING_DATA")
-TEST_DATA = os.environ.get("TEST_DATA")
+TRAINING_DATA_DIR = os.environ.get("TRAINING_DATA")
+TEST_DATA_DIR = os.environ.get("TEST_DATA")
+PARAMS_DIR = os.environ.get("PARAMS")
 FOLD = int(os.environ.get("FOLD"))
 MODEL = os.environ.get("MODEL")
 
-FOLD_MAPPPING = {
+FOLD_MAPPING = {
     0: [1, 2, 3, 4],
     1: [0, 2, 3, 4],
     2: [0, 1, 3, 4],
@@ -21,23 +24,36 @@ FOLD_MAPPPING = {
 }
 
 if __name__ == "__main__":
-    df = pd.read_csv(TRAINING_DATA)
-    df_test = pd.read_csv(TEST_DATA)
-    train_df = df[df.kfold.isin(FOLD_MAPPPING.get(FOLD))].reset_index(drop=True)
-    valid_df = df[df.kfold==FOLD].reset_index(drop=True)
+    with open(PARAMS_DIR) as json_params:
+        params = json.load(json_params)
+    df = pd.read_csv(TRAINING_DATA_DIR)
+    test_df = pd.read_csv(TEST_DATA_DIR)
 
-    ytrain = train_df.target.values
-    yvalid = valid_df.target.values
+    try:
+        train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))].reset_index(drop=True)
+        valid_df = df[df.kfold==FOLD].reset_index(drop=True)
+    except AttributeError:
+        train_df, valid_df = train_test_split(df, test_size=0.2)
 
-    train_df = train_df.drop(["id", "target", "kfold"], axis=1)
-    valid_df = valid_df.drop(["id", "target", "kfold"], axis=1)
+    # ytrain = train_df.target.values
+    # yvalid = valid_df.target.values
+    target = params['target']
+    ytrain = train_df[target]
+    yvalid = valid_df[target]
+
+    # train_df = train_df.drop(["id", "target", "kfold"], axis=1)
+    # valid_df = valid_df.drop(["id", "target", "kfold"], axis=1)
+    train_df = train_df.drop([params['id'], params['target']], axis=1)
+    valid_df = valid_df.drop([params['id'], params['target']], axis=1)
 
     valid_df = valid_df[train_df.columns]
 
     label_encoders = {}
+    print(train_df.dtypes)
     for c in train_df.columns:
+        print(c)
         lbl = preprocessing.LabelEncoder()
-        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist() + df_test[c].values.tolist())
+        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist() + test_df[c].values.tolist())
         train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
         valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
         label_encoders[c] = lbl
